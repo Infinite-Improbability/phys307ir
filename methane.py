fcc835ef66bcaa839e55099ec1e98dc66ee34612
arconyx <arconyx6@gmail.com>
2021-04-17 12:58:28 +1200

    Methane peak detection

--- methane.py
+++ methane.py
@@ -3,6 +3,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import logging as log
+from scipy import signal
 
 # Get the datafile we need
 filename = 'Methane (sample A) high res.csv'
@@ -17,8 +18,14 @@ data.drop(labels=[0, 1, 2], axis='index', inplace=True)
 data = data.astype(float)
 log.info('Cleaned pandas object ' + filename)
 
+# Get peaks
+peak_indices = signal.find_peaks(data['Voltage (uV)'], prominence=8)[0]
+peaks = data.iloc[peak_indices]
+
+# Plot
 fig, ax = plt.subplots()
 ax.plot(data['Wavelength (nm)'], data['Voltage (uV)'])
+ax.scatter(peaks['Wavelength (nm)'], peaks['Voltage (uV)'], color='black')
 ax.set_xlabel('Wavelength (nm)')
 ax.set_ylabel('Voltage ($\mu$V)') # pylint: disable=anomalous-backslash-in-string
 ax.set_title(filename)
